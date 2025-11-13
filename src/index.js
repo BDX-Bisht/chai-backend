f011ca86b44da558510716eb4cbd5eb227d7fd8c
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2025-11-13 22:55:13 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 // require("dotenv").config({path: "./env",});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("Something went srong");
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error !!!!:", err);
+  });
