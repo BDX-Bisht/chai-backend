9b1c8ff4c990f0374376fc754ee5fa04f05c28af
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-01-23 23:21:57 +0530

    add user routes for password change, current user retrieval, and profile updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+    changeCurrentUserPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
     loginUser,
     logOutUser,
     refreshAccessToken,
     registerUser,
+    updateAvatar,
+    updateCoverImage,
+    updateUserDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,5 +30,20 @@ router.route("/login").post(loginUser);
 // ------ Secured Routes ------------
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/changed-password").post(verifyJWT, changeCurrentUserPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").patch(verifyJWT, updateUserDetails);
+
+router
+    .route("/update-avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+    .route("/update-cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
